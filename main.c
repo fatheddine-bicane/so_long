8ad22ee58d5838e378ff2a200f2167368691ec7a
Fath Eddine Bicane <fbicane@e2r9p10.1337.ma>
2025-02-26 11:11:56 +0000

    starting on the bunus part (animation)

--- main.c
+++ main.c
@@ -24,47 +24,12 @@ void	ft_check_file(char *argv)
 
 int	main(int argc, char **argv)
 {
-	/*char	**map;*/
-	/*int		i;*/
 	t_game	*game;
 
 	(void)argc;
-	/*ft_check_file(argv[1]);*/
 	game = malloc(sizeof(t_game));
 	if (!game)
 		return(1);
 	ft_parce_map(argv[1], &game);
 	ft_open_win(&game);
-	/*i = 0;*/
-	/*while (game->map[i])*/
-	/*{*/
-	/*	ft_printf(game->map[i]);*/
-	/*	ft_printf("\n");*/
-	/*	i++;*/
-	/*}*/
-	/*ft_free_game(&game);*/
-	/*printf("\n\n\n\n\n\n\n\n\n");*/
-	/*char	**map_copy = map;*/
-	/*ft_flood_fill(game, &map_copy, 3, 1);*/
-	/*i = 0;*/
-	/*while (map[i])*/
-	/*{*/
-	/*	ft_printf(map[i]);*/
-	/*	ft_printf("\n");*/
-	/*	i++;*/
-	/*}*/
-	/////////////////////////
-	/*game = malloc(sizeof(t_game*));*/
-	/*if (!game)*/
-	/*	return (1);*/
-	/*map = ft_parce_map(argv[1], &game);*/
-	/*i = 0;*/
-	/*while (map[i])*/
-	/*{*/
-	/*	ft_printf(map[i]);*/
-	/*	ft_printf("\n");*/
-	/*	i++;*/
-	/*}*/
-	/*ft_free(map);*/
-	/*printf("\n\n\n\n\n\n%d", game->P_y_pos);*/
 }
--- src_mandatory/main.c
+++ src_mandatory/main.c
@@ -1,29 +1,35 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   so_long.c                                          :+:      :+:    :+:   */
+/*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fbicane <fbicane@student.1337.ma>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2025/02/13 20:41:56 by fbicane           #+#    #+#             */
-/*   Updated: 2025/02/19 12:34:26 by fbicane          ###   ########.fr       */
+/*   Created: 2025/02/15 10:22:42 by fbicane           #+#    #+#             */
+/*   Updated: 2025/02/19 15:47:18 by fbicane          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "so_long.h"
 
-int	main(void)
+void	ft_check_file(char *argv)
 {
-	void	*mlx_ptr;
-	void	*mlx_win;
-	void	*background;
-	int		height;
-	int		width;
+	int	fd;
 
-	/*(void)mlx_win;*/
-	mlx_ptr = mlx_init();
-	mlx_win = mlx_new_window(mlx_ptr, 12 * 32, 5 * 32, "so_long");
-	background = mlx_xpm_file_to_image(mlx_ptr, "./sprites/background_town1.xpm", &width, &height);
-	mlx_put_image_to_window(mlx_ptr, mlx_win, background, 0, 0);
-	mlx_loop(mlx_ptr);
+	fd = open(argv, O_RDONLY);
+	if (fd == -1)
+		ft_throw_error_1(1, NULL);
+	close(fd);
+}
+
+int	main(int argc, char **argv)
+{
+	t_game	*game;
+
+	(void)argc;
+	game = malloc(sizeof(t_game));
+	if (!game)
+		return(1);
+	ft_parce_map(argv[1], &game);
+	ft_open_win(&game);
 }
